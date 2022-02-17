d85bbed9dbb8b69778d4bbd74a2dd199b2f7ccf2
robin nordin <robin.nordin2@gmail.com>
2022-02-17 17:54:18 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const { generateToken, validateToken } = require('./token');
 
